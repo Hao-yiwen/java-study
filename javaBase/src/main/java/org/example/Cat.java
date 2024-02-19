3f93e31432695ddb3885ef9c7dfe25d2aa92969d
yw.hao <yw.hao@trip.com>
2024-02-19 13:44:25 +0800

    feat: add interface abstract instanceof example

---
+++ javaBase/src/main/java/org/example/Cat.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Cat extends Animal {
+    @Override
+    public void test() {
+        System.out.println("dsadas");
+    }
+}
---
+++ javaBase/src/main/java/org/example/RectCicle.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RectCicle extends Circle{
+    @Override
+    public void draw(){
+        System.out.println("dasdsa");
+    }
+}
